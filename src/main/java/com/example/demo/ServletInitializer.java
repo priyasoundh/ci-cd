1efcf8eb5b2c2a0bd57bdbba0c601c1e2a24dd86
priya.s <priyasoundh@gmail.com>
2020-12-12 20:41:38 +0530

    uploaded

---
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ServletInitializer extends SpringBootServletInitializer {
+	
+	@RequestMapping(value="/as")
+	@ResponseBody
+	public String home() {
+		return "welcome priya";
+	}
+
+	//@Override
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	//	return application.sources(SpringGradleApplication.class);
+	//}
+
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,5 @@
+<html>
+	<body>
+		priya
+	</body>
+</html>
